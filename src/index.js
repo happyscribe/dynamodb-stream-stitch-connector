71ed19b49f4a46d5af1d2532eda0566e429c997c
Yonah Forst <yonaforst@hotmail.com>
2019-11-17 17:13:43 +0100

    dynamo sequence numbers can get too large. remove more zeros

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ module.exports.init = (config) => {
         
       const obj = AWS.DynamoDB.Converter.unmarshall(NewImage)
       // dynamodbs have a bunch of zeros in the middle which make the number too large for stitch
-      const sequence = SequenceNumber.replace('0000000', '')
+      const sequence = SequenceNumber.replace('000000000', '')
       return clientInstance.send(obj, sequence)
     }
   }
